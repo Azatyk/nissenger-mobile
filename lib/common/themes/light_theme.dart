de9ad796b2cf6651ff557871acf7bc40b6a2dde5
Azatyk <azatuk2005@mail.ru>
2022-09-05 14:56:08 +0600

    feat: add fonts, theme and color variables

---
+++ lib/common/themes/light_theme.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:nissenger_mobile/common/variables/app_colors.dart';
+
+ThemeData lightTheme() {
+  const String sfProDisplayForm = "SF Pro Display";
+  const String montserratFont = "Montserrat";
+
+  TextTheme _lightTextTheme(TextTheme base) {
+    // для текста таймера
+    return base.copyWith(
+      displayLarge: base.displayLarge?.copyWith(
+        fontFamily: montserratFont,
+        fontSize: 60,
+        fontWeight: FontWeight.w500,
+        color: AppColors.white,
+      ),
+      displaySmall: base.displaySmall?.copyWith(
+        fontFamily: montserratFont,
+        fontSize: 34,
+        fontWeight: FontWeight.bold,
+        color: AppColors.white,
+      ),
+      headlineMedium: base.headlineMedium?.copyWith(
+        fontFamily: montserratFont,
+        fontSize: 26,
+        fontWeight: FontWeight.w600,
+        color: AppColors.textDark,
+      ),
+      headlineSmall: base.headlineSmall?.copyWith(
+        fontFamily: montserratFont,
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+        color: AppColors.textDark,
+      ),
+      titleLarge: base.titleLarge?.copyWith(
+        fontFamily: montserratFont,
+        fontSize: 24,
+        fontWeight: FontWeight.w600,
+        color: AppColors.textDark,
+      ),
+      titleMedium: base.titleMedium?.copyWith(
+        fontFamily: sfProDisplayForm,
+        fontSize: 18,
+        fontWeight: FontWeight.w400,
+        color: AppColors.textSecondary,
+      ),
+      titleSmall: base.titleSmall?.copyWith(
+        fontFamily: montserratFont,
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        color: AppColors.textDark,
+      ),
+      labelLarge: base.labelLarge?.copyWith(
+        fontFamily: sfProDisplayForm,
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: AppColors.textDark,
+      ),
+      labelMedium: base.labelMedium?.copyWith(
+        fontFamily: sfProDisplayForm,
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        color: AppColors.textDark,
+      ),
+      button: base.button?.copyWith(
+        fontFamily: sfProDisplayForm,
+        fontSize: 18,
+        fontWeight: FontWeight.w500,
+        color: AppColors.white,
+      ),
+    );
+  }
+
+  const ColorScheme lightColorScheme = ColorScheme(
+    brightness: Brightness.light,
+    primary: AppColors.primary,
+    onPrimary: AppColors.primaryBackground,
+    secondary: AppColors.textDark,
+    onSecondary: AppColors.textSecondary,
+    background: AppColors.background,
+    onBackground: AppColors.grey,
+    error: AppColors.error,
+    onError: AppColors.errorBackground,
+    surface: AppColors.white,
+    onSurface: AppColors.buttonPrimary,
+  );
+
+  final ThemeData lightTheme = ThemeData.light();
+  return lightTheme.copyWith(
+    textTheme: _lightTextTheme(lightTheme.textTheme),
+    colorScheme: lightColorScheme,
+    textSelectionTheme:
+        const TextSelectionThemeData(cursorColor: AppColors.primary),
+  );
+}
---
+++ lib/common/variables/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const textDark = Color(0xFF2D3436);
+  static const textSecondary = Color(0xFF7D8385);
+  static const white = Color(0xFFFFFFFF);
+  static const background = Color(0xFFF6F8FA);
+  static const grey = Color(0xFFCAD0D1);
+  static const primary = Color(0xFF2A81DD);
+  static const primaryBackground = Color(0xFFBADCFF);
+  static const buttonPrimary = Color(0xFFEEF6FF);
+  static const error = Color(0xFFE74C3C);
+  static const errorBackground = Color(0xFFFFF0EE);
+}
