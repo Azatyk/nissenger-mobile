59095a3a467da1dfea6277e8a4c768b4310fc0a5
Azatyk <azatuk2005@mail.ru>
2022-11-05 18:41:29 +0600

    refactor: replace greeting cubit properties based state to seperated class based states

--- lib/modules/greeting/view/components/greeting_actions.dart
+++ lib/modules/greeting/view/components/greeting_actions.dart
@@ -5,7 +5,6 @@ import 'package:nissenger_mobile/common/components/common_button.dart';
 import 'package:nissenger_mobile/common/types/user_types.dart';
 import 'package:nissenger_mobile/modules/greeting/data/cubit/greeting_cubit.dart';
 import 'package:nissenger_mobile/modules/greeting/data/cubit/greeting_state.dart';
-import 'package:nissenger_mobile/modules/greeting/data/types/greeting_status.type.dart';
 import 'package:nissenger_mobile/modules/onboarding/data/plain_data/slides.dart';
 import 'package:nissenger_mobile/modules/onboarding/data/types/slide.dart';
 import 'package:nissenger_mobile/modules/onboarding/view/pages/onboarding_page.dart';
@@ -18,12 +17,11 @@ class GreetingActions extends StatelessWidget {
     final theme = Theme.of(context);
 
     return BlocListener<GreetingCubit, GreetingState>(
-      listenWhen: (prevState, newState) =>
-          newState.status == GreetingStatus.readyToPush,
+      listenWhen: (prevState, newState) => newState is GreetingReadyToPush,
       listener: (context, state) {
         List<Slide> slides = [];
 
-        if (state.userType == UserTypes.teacher) {
+        if ((state as GreetingReadyToPush).userType == UserTypes.teacher) {
           slides = SlidesData.teacherSlides;
         } else if (state.userType == UserTypes.student) {
           slides = SlidesData.studentSlides;
