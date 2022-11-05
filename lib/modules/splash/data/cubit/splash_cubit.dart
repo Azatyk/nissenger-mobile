8021ed273ed3086e907f0d4b11a4a5eff35f1780
Azatyk <azatuk2005@mail.ru>
2022-11-05 18:36:43 +0600

    refactor: change splash cubit state from status property to seperated classes and add text under loading

--- lib/modules/splash/data/cubit/splash_cubit.dart
+++ lib/modules/splash/data/cubit/splash_cubit.dart
@@ -2,20 +2,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:nissenger_mobile/config/hive_boxes.dart';
 import 'package:nissenger_mobile/modules/splash/data/cubit/splash_state.dart';
-import 'package:nissenger_mobile/modules/splash/data/types/splash_status.dart';
 
 class SplashCubit extends Cubit<SplashState> {
   SplashCubit()
-      : super(const SplashState(
-          status: SplashStatus.loading,
-        ));
+      : super(
+          const SplashStateLoading(),
+        );
 
   void initializeApp() async {
     await Hive.initFlutter();
     await Hive.openBox(UserSettingsBox.boxName);
 
     emit(
-      const SplashState(status: SplashStatus.readyToPush),
+      const SplashStateReadyToPush(),
     );
   }
 }
