eb23f7910fc4cbdfac41ec613b58ef551002730b
Azatyk <azatuk2005@mail.ru>
2022-11-19 22:44:55 +0600

    feat: add changing timer page background color depend on state

--- lib/modules/timer/view/pages/timer_page.dart
+++ lib/modules/timer/view/pages/timer_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nissenger_mobile/modules/timer/data/helpers/ticker.dart';
 import 'package:nissenger_mobile/modules/timer/data/timer_cubit/timer_cubit.dart';
+import 'package:nissenger_mobile/modules/timer/data/timer_cubit/timer_state.dart';
+import 'package:nissenger_mobile/modules/timer/data/types/timer_types.dart';
 import 'package:nissenger_mobile/modules/timer/view/components/timer_page_back_button.dart';
 import 'package:nissenger_mobile/modules/timer/view/components/timer_page_timer.dart';
 
@@ -11,29 +13,46 @@ class TimerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TimerCubit(ticker: const Ticker()),
+      child: const TimerPageContent(),
+    );
+  }
+}
+
+class TimerPageContent extends StatelessWidget {
+  const TimerPageContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
 
-    return Scaffold(
-      backgroundColor: theme.colorScheme.primary,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 25.w,
-            vertical: 28.h,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const TimerPageBackButton(),
-              SizedBox(height: 80.h),
-              BlocProvider(
-                create: (context) => TimerCubit(ticker: const Ticker()),
-                child: const Center(
+    return BlocBuilder<TimerCubit, TimerState>(
+      builder: (context, state) => Scaffold(
+        backgroundColor: (state is TimerDiactive &&
+                state.type == TimerDiactiveTypes.beforeLessons)
+            ? const Color(0xFFFF9F43)
+            : (state is TimerDiactive &&
+                    state.type == TimerDiactiveTypes.afterLessons)
+                ? const Color(0xFF1EAE62)
+                : theme.colorScheme.primary,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 25.w,
+              vertical: 28.h,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const TimerPageBackButton(),
+                SizedBox(height: 80.h),
+                const Center(
                   child: TimerPageTimer(),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
