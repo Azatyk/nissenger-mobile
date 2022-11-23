685f34ee5edb88e32887ae6fb8ab9769295ae8ec
Azatyk <azatuk2005@mail.ru>
2022-11-23 08:59:26 +0600

    feat: add get timetable request

---
+++ lib/data/data_providers/dto/get_timetable.dto.dart
@@ -0,0 +1,38 @@
+import 'package:nissenger_mobile/data/models/class.model.dart';
+import 'package:nissenger_mobile/data/models/school.model.dart';
+
+class GetTimetableDto {
+  final School school;
+  final Class className;
+  final String group;
+  final List<String>? profileGroups;
+  final String? foreignLanguage;
+
+  const GetTimetableDto({
+    required this.school,
+    required this.className,
+    required this.group,
+    this.profileGroups,
+    this.foreignLanguage,
+  });
+
+  Map<String, dynamic> toJson() {
+    var map = {
+      "school": school.toJson(),
+      "class": className.toJson(),
+      "group": group,
+    };
+
+    if (profileGroups != null) {
+      map["profile_groups"] = [
+        profileGroups?.map((profileGroup) => profileGroup).toList(),
+      ];
+    }
+
+    if (foreignLanguage != null) {
+      map["foreign_language"] = foreignLanguage!;
+    }
+
+    return map;
+  }
+}
---
+++ lib/data/data_providers/requests/timetable_requests.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:nissenger_mobile/data/data_providers/dto/get_timetable.dto.dart';
+import 'package:nissenger_mobile/utils/dio.util.dart';
+
+class TimetableRequests {
+  static Future getTimetable({required GetTimetableDto getTimetableDto}) async {
+    Response res = await DioUtil.getInstance().post(
+      "/timetable/lessons",
+      data: getTimetableDto.toJson(),
+    );
+
+    return res;
+  }
+}
