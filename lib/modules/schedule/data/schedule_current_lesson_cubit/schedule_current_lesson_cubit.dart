7d84d738c8919d0dba86cc1df8264027a24dd115
Azatyk <azatuk2005@mail.ru>
2022-11-30 15:29:15 +0600

    fix: fix bug of wrong showing active lesson in schedule page

--- lib/modules/schedule/data/schedule_current_lesson_cubit/schedule_current_lesson_cubit.dart
+++ lib/modules/schedule/data/schedule_current_lesson_cubit/schedule_current_lesson_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nissenger_mobile/common/helpers/time_checker.dart';
 import 'package:nissenger_mobile/data/models/lesson.model.dart';
 import 'package:nissenger_mobile/modules/schedule/data/schedule_current_lesson_cubit/schedule_current_lesson_state.dart';
 
@@ -6,24 +7,11 @@ class ScheduleCurrentLessonCubit extends Cubit<ScheduleCurrentLessonState> {
   ScheduleCurrentLessonCubit() : super(const ScheduleCurrentLessonPure());
 
   void checkActiveLesson({required List<Lesson> todayLessons}) {
-    DateTime currentTime = DateTime.now();
-
     for (int i = 0; i < todayLessons.length; i++) {
-      if (currentTime.hour.toInt() == todayLessons[i].time.startTimeHour) {
-        if (currentTime.hour.toInt() == todayLessons[i].time.endTimeHour &&
-                currentTime.minute.toInt() <
-                    todayLessons[i].time.endTimeMinute ||
-            currentTime.hour.toInt() < todayLessons[i].time.endTimeHour) {
-          emit(
-            ScheduleCurrentLessonIndex(index: i),
-          );
-        }
-      } else if (currentTime.hour.toInt() == todayLessons[i].time.endTimeHour) {
-        if (currentTime.minute.toInt() < todayLessons[i].time.endTimeMinute) {
-          emit(
-            ScheduleCurrentLessonIndex(index: i),
-          );
-        }
+      if (TimeChecker.isCurrentTimeInLesson(lesson: todayLessons[i])) {
+        emit(ScheduleCurrentLessonIndex(index: i));
+
+        break;
       }
     }
   }
