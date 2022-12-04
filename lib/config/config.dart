92ed9707d7ded8ba66ad421a8498c111251eb303
Azatyk <azatuk2005@mail.ru>
2022-12-04 20:52:13 +0600

    feat: add publish settings and app metrica

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -4,4 +4,5 @@ class Config {
   static String apiUrl = dotenv.env["API_URL"] ?? "";
   static String requestCity = dotenv.env["REQUEST_CITY"] ?? "";
   static String requestSchool = dotenv.env["REQUEST_SCHOOL"] ?? "";
+  static String appMetricaKey = dotenv.env["APP_METRICA_KEY"] ?? "";
 }
