4bf75300576116aa8dae3bf5ca8e3b88a62bffc2
Azatyk <azatuk2005@mail.ru>
2022-12-21 08:37:18 +0600

    feat: change separator from dashed to line

--- lib/common/components/dashed_divider.dart
+++ lib/common/components/dashed_divider.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DashedDivider extends StatelessWidget {
   const DashedDivider({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        final boxWidth = constraints.constrainWidth();
-        const dashWidth = 20.0;
-        const dashHeight = 1.0;
-        final dashCount = (boxWidth / (1.5 * dashWidth)).floor();
-        return Flex(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          direction: Axis.horizontal,
-          children: List.generate(dashCount, (_) {
-            return const SizedBox(
-              width: dashWidth,
-              height: dashHeight,
-              child: DecoratedBox(
-                decoration: BoxDecoration(color: Color(0xFFCACACA)),
-              ),
-            );
-          }),
-        );
-      },
+    ThemeData theme = Theme.of(context);
+
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10.h),
+      width: double.infinity,
+      height: 1.h,
+      color: theme.colorScheme.onBackground.withOpacity(0.7),
     );
+    // return LayoutBuilder(
+    //   builder: (BuildContext context, BoxConstraints constraints) {
+    //     final boxWidth = constraints.constrainWidth();
+    //     const dashWidth = 20.0;
+    //     const dashHeight = 1.0;
+    //     final dashCount = (boxWidth / (1.5 * dashWidth)).floor();
+    //     return Flex(
+    //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //       direction: Axis.horizontal,
+    //       children: List.generate(dashCount, (_) {
+    //         return const SizedBox(
+    //           width: dashWidth,
+    //           height: dashHeight,
+    //           child: DecoratedBox(
+    //             decoration: BoxDecoration(color: Color(0xFFCACACA)),
+    //           ),
+    //         );
+    //       }),
+    //     );
+    //   },
+    // );
   }
 }
