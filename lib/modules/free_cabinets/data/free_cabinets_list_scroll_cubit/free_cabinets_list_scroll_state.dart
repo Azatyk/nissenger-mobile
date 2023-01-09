998f33c899d5266019f7ce6a6d1d346b3dbfda58
NjrexUI <nurakhmet.aidarbek@gmail.com>
2023-01-09 22:39:04 +0600

    feat: dividers on bottom and top add

---
+++ lib/modules/free_cabinets/data/free_cabinets_list_scroll_cubit/free_cabinets_list_scroll_state.dart
@@ -0,0 +1,11 @@
+abstract class FreeCabinetsListScrollState {
+  const FreeCabinetsListScrollState();
+}
+
+class FreeCabinetsListScrollHideBorder extends FreeCabinetsListScrollState {
+  const FreeCabinetsListScrollHideBorder();
+}
+
+class FreeCabinetsListScrollShowBorder extends FreeCabinetsListScrollState {
+  const FreeCabinetsListScrollShowBorder();
+}
--- lib/modules/free_cabinets/view/components/free_cabinets_list.dart
+++ lib/modules/free_cabinets/view/components/free_cabinets_list.dart
@@ -4,15 +4,80 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nissenger_mobile/common/components/common_choice_button.dart';
 import 'package:nissenger_mobile/common/components/error_snackbar.dart';
+import 'package:nissenger_mobile/modules/free_cabinets/data/free_cabinets_list_scroll_cubit/free_cabinets_list_scroll_cubit.dart';
+import 'package:nissenger_mobile/modules/free_cabinets/data/free_cabinets_list_scroll_cubit/free_cabinets_list_scroll_state.dart';
 import 'package:nissenger_mobile/modules/free_cabinets_schedule/data/free_cabinet_schedule_cubit/free_cabinets_schedule_cubit.dart';
 import 'package:nissenger_mobile/modules/free_cabinets/data/free_cabinets_list_cubit/free_cabinets_list_cubit.dart';
 import 'package:nissenger_mobile/modules/free_cabinets/data/free_cabinets_list_cubit/free_cabinets_list_state.dart';
 import 'package:nissenger_mobile/modules/free_cabinets_schedule/view/pages/free_cabinets_schedule_page.dart';
 
-class FreeCabinetsList extends StatelessWidget {
+class FreeCabinetsList extends StatefulWidget {
   const FreeCabinetsList({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<FreeCabinetsList> createState() => _FreeCabinetsListState();
+}
+
+class _FreeCabinetsListState extends State<FreeCabinetsList>
+    with WidgetsBindingObserver {
+  late ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    controller = ScrollController();
+    controller.addListener(scrollListener);
+
+    BlocProvider.of<FreeCabinetsListScrollCubit>(context).reachTop();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      scrollToTop();
+    });
+
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  void scrollListener() {
+    var currentState = context.read<FreeCabinetsListScrollCubit>().state;
+
+    if (controller.offset > controller.position.minScrollExtent &&
+        !controller.position.outOfRange &&
+        currentState is FreeCabinetsListScrollHideBorder) {
+      BlocProvider.of<FreeCabinetsListScrollCubit>(context, listen: false)
+          .scrollToBottom();
+    } else if (controller.offset <= controller.position.minScrollExtent &&
+        currentState is FreeCabinetsListScrollShowBorder) {
+      BlocProvider.of<FreeCabinetsListScrollCubit>(context, listen: false)
+          .reachTop();
+    }
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      scrollToTop();
+    }
+
+    super.didChangeAppLifecycleState(state);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+
+    super.dispose();
+  }
+
+  void scrollToTop() {
+    if (controller.hasClients) {
+      controller.animateTo(
+        0,
+        duration: const Duration(milliseconds: 1),
+        curve: Curves.bounceInOut,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,6 +116,7 @@ class FreeCabinetsList extends StatelessWidget {
             children: [
               Expanded(
                 child: SingleChildScrollView(
+                  controller: controller,
                   physics: const BouncingScrollPhysics(),
                   child: Wrap(
                     spacing: 12.r,
