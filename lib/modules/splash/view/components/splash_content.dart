ffc726521df5cc0515ef2ec506fc2e8e083084e1
NjrexUI <nurakhmet.aidarbek@gmail.com>
2023-01-16 01:31:26 +0600

    fix: splash page content error type add

--- lib/modules/splash/view/components/splash_content.dart
+++ lib/modules/splash/view/components/splash_content.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nissenger_mobile/common/components/error_block.dart';
+import 'package:nissenger_mobile/helpers/error_messages.dart';
 import 'package:nissenger_mobile/modules/splash/data/cubit/splash_cubit.dart';
 import 'package:nissenger_mobile/modules/splash/data/cubit/splash_state.dart';
-
-import '../../../../common/cubits/support_cubit/support_cubit.dart';
-import '../../../../common/modals/support.modal.dart';
 
 class SplashContent extends StatelessWidget {
   const SplashContent({super.key});
@@ -20,35 +18,14 @@ class SplashContent extends StatelessWidget {
         if (state is SplashStateInternetConnectionError ||
             state is SplashStateUnknownError) {
           return ErrorBlock(
+            errorType: state is SplashStateInternetConnectionError
+                ? ErrorTypes.internetConnectionError
+                : ErrorTypes.unknownError,
             reverseColor: true,
-            title: state is SplashStateInternetConnectionError
-                ? "Нет интернета"
-                : "Что-то пошло не так",
-            subtitle: state is SplashStateInternetConnectionError
-                ? "Проверьте подключение и попробуйте снова"
-                : "Попробуйте обновить или напишите нам, мы разберемся",
-            mainButtonText: "Обновить",
             onMainButtonPressed: () {
               BlocProvider.of<SplashCubit>(context).initializeApp();
             },
             secondaryButton: true,
-            secondaryButtonText: "Написать нам",
-            onSecondaryButtonPressed: () {
-              showModalBottomSheet(
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(
-                    20.r,
-                  )),
-                ),
-                context: context,
-                builder: (context) => BlocProvider(
-                  create: (context) => SupportCubit(),
-                  child: const SupportMethodsModal(),
-                ),
-              );
-            },
           );
         } else if (state is SplashStateLoading) {
           return Column(
