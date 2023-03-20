0370df51cd1ca8fabbb343071a50acd893067dc2
NjrexUI <nurakhmet.aidarbek@gmail.com>
2023-03-20 12:53:48 +0600

    fix: appMetrica uncomment

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ import 'config/preset_hive_class.dart';
 void main() async {
   await dotenv.load(fileName: ".env");
 
-  // AppMetrica.activate(AppMetricaConfig(Config.appMetricaKey));
+  AppMetrica.activate(AppMetricaConfig(Config.appMetricaKey));
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   // HttpOverrides.global = MyHttpOverrides();
